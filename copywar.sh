611938d281ddf19de15cd738053161a19b0ca0ba
yogisdm <31575108+yogisdm@users.noreply.github.com>
2019-06-08 22:50:24 +0530

    code update

--- copywar.sh
+++ copywar.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-$key = aws s3 ls s3://dd-cdo-poc/uc1-loginwebapp/target --recursive | sort | tail -n 1 | awk '{print $4}'
+key=$(aws s3 ls s3://dd-cdo-poc/uc1-loginwebapp/target --recursive | sort | tail -n 1 | awk '{print $4}')
 aws s3 cp s3://dd-cdo-poc/$key /tmp/LoginWebApps.war
